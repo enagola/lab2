7054dea94427a73c7eb7cc3a18fc053cc6281cea
Ethan Nagola <enagola@ucsd.edu>
2021-01-14 10:45:41 -0800

    added the js stuff :)

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,8 +2,15 @@ $(document).ready(function() {
     initializePage();
 });
 
+function projectClick(e) {
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+}
+
 function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
-            });
+          });
+    $("a.thumbnail").click(projectClick);
+
 }
